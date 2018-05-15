0ce30048e938a3bd727e056457a2156bcfd7a18f
Doan Ngoc Nam <nam.dng2511@gmail.com>
2018-05-15 20:33:08 +0700

    fix bootstrap error

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+    def new
+    end
+  
+    def index
+      @books = Book.paginate_by_sql("select * from books where is_deleted = 'f'", :page => params[:page], :per_page => 12)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home' => 'home#index', as: :home
   root to: 'visitors#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users
