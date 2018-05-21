e0832918940c63cfa90b3dae43143cb06a0a7711
Tuấn Dũng <zun1903@gmail.com>
2018-05-21 23:00:45 +0700

    add phone & address to edit pro5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone, :admin])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :phone])
   end
 
 end
