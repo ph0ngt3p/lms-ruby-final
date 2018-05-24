4d614726943b20078e2ebdc777662ba71d1fb6fe
Tuấn Dũng <zun1903@gmail.com>
2018-05-24 11:19:21 +0700

    refactor search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,6 @@
 class Search < ApplicationRecord
   def search_books
-    books = Book.all
-    books = books.where("isbn LIKE ? and title LIKE ? and publisher LIKE ? and author LIKE ? and is_deleted = ?", "%#{isbn}%", "%#{title}%", "%#{publisher}%", "%#{author}%", false)
+    books = Book.where("isbn LIKE ? and title LIKE ? and publisher LIKE ? and author LIKE ? and is_deleted = ?", "%#{isbn}%", "%#{title}%", "%#{publisher}%", "%#{author}%", false)
     return books
   end
 end
